4fb5829d5126f56c0f940455481f70397a696da6
LexzTemptation <alejandro_temptation@outlook.com>
2023-03-21 22:36:13 -0600

    Estilos de react

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -3,15 +3,20 @@ import React, { Fragment } from "react";
 export default function CC() {
     return (
         <Fragment>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="/app">Login</a>
-                    </li>
-                    <li>
-                        <a href="/">Inicio</a>
-                    </li>
-                </ul>
+            <nav class="navbar navbar-expand-lg bg-danger">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">Home</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/app">Iniciar sesion</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
             <div class="container-fluid">
                 <br />
@@ -25,37 +30,39 @@ export default function CC() {
                                         <h4>LLENE TODOS LOS CAMPOS</h4>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="text" placeholder="Nombre(s)" />
+                                            <input class="inpt-ca" type="text" placeholder="Nombre(s)" />
                                         </div>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="text" placeholder="Apellido paterno" />
+                                            <input class="inpt-ca" type="text" placeholder="Apellido paterno" />
                                         </div>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="password" placeholder="Apellido materno" />
+                                            <input class="inpt-ca" type="password" placeholder="Apellido materno" />
                                         </div>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="date" placeholder="Fecha de nacimiento" />
+                                            <input class="inpt-ca" type="date" placeholder="Fecha de nacimiento" />
                                         </div>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="tel" placeholder="Teléfono" />
+                                            <input class="inpt-ca" type="tel" placeholder="Teléfono" />
                                         </div>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="email" placeholder="Email" />
+                                            <input class="inpt-ca" type="email" placeholder="Email" />
                                         </div>
                                         <br /><br />
                                         <div class="col-9">
-                                            <input class="inpt-cc" type="password" placeholder="Contraseña" />
+                                            <input class="inpt-ca" type="password" placeholder="Contraseña" />
                                         </div>
                                     </div>
                                     <br />
-                                    <div class="row justify-content-center">
-                                        <div class="col-12">
-                                            <button class="btnCreateAccnt" >Crear cuenta</button>
+                                    <div class="row justify-content-left">
+                                        <div class="col-9">
+                                            <div class="d-grid gap-2">
+                                                <button id="btnCreateAccnt" class="btn btn-danger btn-lg" >Crear cuenta</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </form>
