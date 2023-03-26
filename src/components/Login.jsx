eb6d701ce5f615e918d19aab6c9881564ce2147f
LexzTemptation <alejandro_temptation@outlook.com>
2023-03-26 14:02:29 -0600

    Login finalizado con zoom al 100%

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,37 +15,44 @@ export default function Login() {
                 </div>
                 <div class="col-12">
                     <div class="row justify-content-center">
-                        <div class="col-5">
+                        <div class="col-7">
                             <br /><br /><br />
                             <div class="card">
-                                <form class="w3-container">
+                                <div class="row justify-content-center">
+                                    <div class="col-4">
+                                        <img src="user-icn.png" width="245" height="245" alt="" />
+                                    </div>
+                                </div>
+                                <form>
                                     <div class="row justify-content-center">
                                         <div class="col-5">
-                                            <img src="user-icn.png" width="250" height="250" alt="" />
-                                        </div>
-                                        <div class="col-8">
                                             <input type="text" placeholder="Email, teléfono, usuario" />
                                         </div>
-                                        <br /><br />
-                                        <div class="col-8">
+                                    </div>
+                                    <br />
+                                    <div class="row justify-content-center">
+                                        <div class="col-5">
                                             <input type="password" placeholder="Contraseña" />
                                         </div>
+                                    </div>
+                                    <br />
+                                    <div class="row justify-content-center">
                                         <br /><br />
-                                        <div class="col-5">
+                                        <div class="col-4">
                                             <button class="login" >Iniciar sesion</button>
-                                            &nbsp;&nbsp;&nbsp;
+                                            &nbsp;&nbsp;
                                             <a href="/createaccount" class="createAccount">Crear cuenta</a>
                                         </div>
-                                        <br /><br /><br />
-                                        <div class="col-8">
+                                    </div>
+                                        <br /><br />
+                                    <div class="row justify-content-center">
+                                        <div class="col-2">
                                             <div style={{ fontSize: "1em", color: "#006BE4" }}>
                                                 <FontAwesomeIcon icon={faCheckSquare} />&nbsp;&nbsp;&nbsp;{"Recordar"}
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                &nbsp;
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div style={{ fontSize: "1em", color: "#006BE4" }}>
                                                 <a href="/">¿Olvidaste tu contraseña?</a>
                                             </div>
                                         </div>
