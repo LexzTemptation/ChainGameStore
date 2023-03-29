f4df91d8f9e5af255fa20e5ea2959e83415cc786
LexzTemptation <alejandro_temptation@outlook.com>
2023-03-28 21:37:57 -0600

    Archivos listos

---
+++ src/components/CreateAccount.jsx
@@ -0,0 +1,102 @@
+import React, { Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+export default function CC() {
+    return (
+        <Fragment>
+            <div class="bdyLogin">
+                <div class="container-fluid">
+                    <br />
+                    <div class="row justify-content-center">
+                        <div class="col-8">
+                            <div style={{ fontSize: "1em", color: "whitesmoke" }}>
+                                <a href="/login"><FontAwesomeIcon icon={faArrowLeft} />{" Regresar"}</a>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <div class="row justify-content-center">
+                        <div class="col-10">
+                            <div class="card">
+                                <br /><br />
+                                <div class="row justify-content-center">
+                                    <div class="col-3">
+                                        <h4>LLENE TODOS LOS CAMPOS</h4>
+                                    </div>
+                                </div>
+                                <div class="row justify-content-center">
+                                    <div class="col-7">
+                                        <br /><br />
+                                        <form>
+                                            <div class="row justify-content-center">
+                                                <br /><br />
+                                                <div class="col">
+                                                    <input class="inpt-ca" type="text" placeholder="Nombre(s)" />
+                                                </div>
+                                                <br /><br />
+                                                <div class="col">
+                                                    <input class="inpt-ca" type="text" placeholder="Apellido paterno" />
+                                                </div>
+                                                <br /><br />
+                                                <div class="col">
+                                                    <input class="inpt-ca" type="text" placeholder="Apellido materno" />
+                                                </div>
+                                            </div>
+                                            <div class="row justify-content-center">
+                                                <br /><br />
+                                                <div class="col">
+                                                    <input class="inpt-ca" type="date"/>
+                                                </div>
+                                                <br /><br />
+                                                <div class="col">
+                                                    <input class="inpt-ca" type="tel" placeholder="Teléfono" />
+                                                </div>
+                                                <br /><br />
+                                                <div class="col-12">
+                                                    <div class="row justify-content-center">
+                                                        <div class="col">
+                                                            <input class="inpt-ca" type="email" placeholder="Email" />
+                                                        </div>
+                                                        <div class="col">
+                                                            <input class="inpt-ca" type="password" placeholder="Contraseña" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <br /><br />
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <br /><br />
+                                <div class="row justify-content-center">
+                                    <div class="col-3">
+                                        <a href class="btnCreateAccnt" >Crear cuenta</a>
+                                    </div>
+                                </div>
+                                <br /><br /><br /><br /><br />
+                            </div> {/* card */}
+                            <br /><br /><br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import ReactDOM from "react-dom";
+import './index.css';
+import App from './App';
+import Login from "./components/Login";
+import CreateAccount from "./components/CreateAccount";
+//import { App } from './routes/App';
+//import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import { CrearCuenta } from './CrearCuenta';
+//import { Link } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/createaccount",
+    element: <CreateAccount />,
+  },
+]);
+
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
+
+
+
+
+
+
+
+
+
