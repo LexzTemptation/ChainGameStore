8b7aa2ac4d0783a22a5324a469d5cd718d9eb1b9
LexzTemptation <alejandro_temptation@outlook.com>
2023-04-10 18:32:18 -0600

    SQL Inserts

---
+++ INSERTS.sql
@@ -0,0 +1,181 @@
+use games;
+select * from producto;
+-- --------------------------------- INSERTS -------------------------------------
+
+-- -------------------Producto-----------------------------------
+
+INSERT INTO Producto VALUE(1, "God Of War 4", 1179, "nuevo", "PlayStation", "1 año", "
+Kratos vuelve a empezar. Kratos, que vive como un hombre lejos de la sombra de los dioses, se adentrará en la 
+salvaje tierra nórdica junto a 
+su hijo Atreus, y ambos lucharán para completar una misión profundamente personal.
+", "SONY", "20 de Abril 2018");
+
+INSERT INTO Producto VALUE(2, "Resident Evil 4 (remake)", 1499, "preventa", "PlayStation, Xbox", "1 año", "
+Resident Evil 4 es un remake del Resident Evil 4 original del 2005.
+
+Survival Horror de última generación reimaginado para el año 2023.
+
+Resident Evil 4 conserva la esencia del juego original, a la vez que introduce mecánicas de juego actualizadas,
+una historia reimaginada, e impresionantes gráficos de última generación que lo convierten
+en la experiencia de Survival Horror definitiva en la cual se cruzan la vida y la muerte, el terror y la catarsis.
+", "CAMPCOM", "24 de Marzo 2023");
+
+INSERT INTO Producto VALUE(3, "CONSOLA PLAYSTATION 5 BUNDLE GOD OF WAR RAGNAROK", 15299, "nuevo", "PlayStation 5", "5 años", 
+"Únete a Kratos y Atreus en un viaje mítico en busca de respuestas y aliados antes de que Ragnarök llegue. 
+Con el telón de fondo de los reinos nórdicos destruidos, sé testigo de la dinámica cambiante de la relación padre-hijo mientras se
+ preparan para la guerra.", "SONY", "30 de Noviembre 2022");
+ 
+INSERT INTO Producto VALUE(4, "CONSOLA XBOX SERIES S", 7599, "nuevo", "XSX", "3 años", 
+ "Disfruta de una velocidad y rendimiento de nueva generación con nuestra biblioteca digital más grande hasta la fecha. 
+ Disfruta de mundos más dinámicos y tiempos de carga más rápidos, y agrega Xbox Game Pass Ultimate (la suscripción se vende por separado) 
+ para jugar a títulos nuevos el mismo día de su lanzamiento. Además, disfruta de cientos de juegos de alta calidad.", "MICROSOFT",
+ "10 de Noviembre 2020");
+ 
+INSERT INTO Producto VALUE (5, "Consola PlayStation de 1 TB edición Bundle God of War Ragnarok", 9499, "nuevo", "PS4", "3 años", 
+ "Disfruta de una PS4 más estilizada y compacta, entrega la energía de juego impresionante que siempre es para los jugadores. 
+ Tambien, disfruta de colores increíblemente vivos y brillantes con los asombrosos gráficos HDR.
+
+Un 30% más delgada y un 16% más ligera que el modelo de PS4 original.", "SONY", "16  de Diciembre 2022");
+
+INSERT INTO Producto VALUE (6, "Control joystick inalámbrico Microsoft Xbox Wireless Controller Series X|S electric volt
+", 1699, "nuevo", "Xbox", "1 año", "
+Xbox Wireless Controller – Electric Volt
+
+-Experimente el diseño modernizado del controlador inalámbrico Xbox con superficies esculpidas y geometría refinada para mayor comodidad y control sin esfuerzo durante el juego.
+– Manténgase en el objetivo con agarre texturizado en los gatillos, parachoques y carcasa trasera y con un nuevo D-pad híbrido para una entrada precisa pero familiar.
+– Personaliza la asignación de botones con la aplicación Accesorios de Xbox para personalizar el mando.
+-Conecte cualquier auricular compatible con el conector para auriculares de audio de 3,5 mm.
+-Incluye tecnología Xbox Wireless y Bluetooth® para juegos inalámbricos en consolas compatibles, PC con Windows 10, teléfonos Android e iOS y tabletas.
+
+Información importante:
+
+-Compatible con ciertos dispositivos y versiones de sistemas operativos. Al",
+"MICROSOFT", "8 de Mayo 2021");
+
+INSERT INTO Producto VALUE(7, "CONTROL ALAMBRICO XBOX ONE NACON PRO", 399, "nuevo", "Xbox", "2 año", "
+
+– Diseño compacto con botones de gran tamaño para mayor comodidad, tiempos de reacción más rápidos y menor fatiga de la mano.
+– Libere audio 3D al instante en cualquier consola Xbox accediendo a Dolby Atmos® para auriculares cuando conecte el controlador.
+– Botones de mapa, ajuste la sensibilidad de la palanca del pulgar, ajuste las zonas muertas de activación y mucho más con una aplicación dedicada que permite opciones de personalización avanzadas.
+– Cableado para reducir la latencia, lo que permite tiempos de reacción más rápidos.
+– Joysticks cóncavos con superficie de alto agarre para proporcionar un control de movimiento mejorado.
+– Cable trenzado de nylon largo de 9.8FT/3M que es muy duradero y ultra flexible.
+– El agarre de alto rendimiento proporciona una comodidad superior y un control preciso.
+– Toma de auriculares de 3,5 mm para funciones de audio y chat (auriculares no incluidos)
+", "NANOSTDAD", "11 de Abril 2022");
+
+INSERT INTO Producto VALUE (8, "CONTROL PLAYSTATION 5 DUALSENSE STARLIGHT BLUE", 1649, "nuevo", "Playstation 5", "1 año", "
+El control inalámbrico DualSense para PS5 ofrece respuesta háptica inmersiva2, gatillos adaptativos dinámicos y un micrófono integrado, todo en un diseño icónico.
+
+Respuesta háptica
+Siente la respuesta física a tus acciones en el juego con los accionadores dobles que reemplazan a los tradicionales motores de vibración. En tus manos, estas vibraciones dinámicas pueden simular la sensación de todo, desde los entornos hasta el retroceso de diferentes armas.
+
+Gatillos adaptativos
+Experimenta la tensión y la fuerza variadas cuando interactúes con los entornos y los equipos del juego. Desde tensar la cuerda de un arco cada vez más hasta pisar los frenos de un automóvil a gran velocidad, siéntete físicamente conectado a tus acciones en la pantalla.
+", "SONY", "17 de Enero 2022" );
+
+INSERT INTO Producto VALUE (9, "BASE DE CARGA PARA CONTROLES PLAYSTATION REDLEMON", 499, "nuevo", "PlayStation", "2 años", "
+Carga 2 controles de Playstation 5 al mismo tiempo:
+
+No esperarás mucho tiempo para jugar tus videojuegos ya que cuenta con dos bases para controles de PlayStation 5 para que puedas cargarlos más rápido. ¡Ahorra tiempo y disfruta de tus videojuegos favoritos con un amigo!
+
+Recarga la batería en 3 horas:
+
+En tan sólo 3 horas, los dos controles estarán totalmente cargados. Conecta la base de carga mediante USB-C a la consola y en menos de lo que te imaginas estarás jugando de nuevo.
+
+", "REDLEMON", "27 de enero 2023");
+
+INSERT INTO Producto VALUE (10, "BASE DE CARGA CONTROL XBOX VOLTEDGE BX40", 799, "nuevo", "Xbox","2 años", "
+El cargador Xbox Voltedge es un dispositivo diseñado para cargar baterías de controles inalámbricos de Xbox. Este cargador utiliza una tecnología de carga rápida que puede durar hasta 15 horas por batería. El cargador tiene una base compacta y elegante que se conecta alámbricamente. También viene con 4 baterías recargables de alta capacidad que se ajustan perfectamente a los controles inalámbricos de Xbox.
+
+También tiene luces indicadoras de estado que muestran cuándo las baterías están completamente cargadas y listas para usar. Además, el cargador también cuenta con un circuito protector para evitar sobre cargas, lo que ayuda a prolongar su vida útil.
+
+Es una excelente opción y confiable para cargar las baterías de tu control inalámbrico de Xbox.
+
+", "VOLTEDGE", "3 de marzo 2023");
+
+INSERT INTO Producto VALUE(11, "BASE DE CARGA PARA JOY CON NINTENDO SWITCH REDLEMON", 399, "nuevo", "Nintendo", "1 año", "
+
+Increíble capacidad de carga:
+
+Posee cuatro ranuras para cargar hasta dos pares de Joy-Con simultáneamente. Sólo desliza los controles en los rieles de carga y no detengas la diversión. Utilízalo para jugar con un amigo en tu Nintendo Switch o Modelo OLED.
+
+Conexión vía USB:
+
+Es increíblemente fácil de utilizar, introduce el conector USB en el puerto del dock o en un adaptador de corriente, desliza los controles en las ranuras y estarán listos para jugar en un par de horas.
+
+Diseño práctico e innovador:
+
+El tamaño compacto te permite llevarlo siempre contigo o convertir la sala de juegos en un verdadero espacio gamer. Mantén los Joy-Con con la carga suficiente y ordenados para no perderlos de vista.
+", "REDLEMON", "27 de febrero 2023");
+
+-- -------------------------VIDEOJuego-------------------------------
+
+INSERT INTO VideoJuego VALUE (1, "Desarrollado por SIE Santa Monica Studio", "Adultos mayores de 18 años", "accion, aventura, rpg",
+"https://www.youtube.com/embed/AN3jEjjcZ-k", 1);
+
+INSERT INTO VideoJuego VALUE (2, "CAPCOM", "Adultos mayores de 18 años", "accion, terror", "https://www.youtube.com/embed/E69tKrfEQag", 2);
+
+-- -----------------------Consola--------------------------------------
+
+INSERT INTO Consola VALUE (2, "825 GB", true, "3840 px x 2160 px", "16GB", "GDDR6 ", 3);
+
+INSERT INTO Consola VALUE (1, "512 GB", true, " 2560 px x 1440 px.", "10 GB", "GDDR6 ", 4);
+
+INSERT INTO Consola VALUE (3, "1 TB", true, "1920 px x 1080 px", "8 GB", "GDDR6", 5);
+
+
+-- -----------------------CONTROL-------------------------------------
+
+INSERT INTO Control VALUE(1, true, "verde", "Jack 3.5 mm, USB-C", true, true, 6);
+
+INSERT INTO Control VALUE (2, false, "negro", "1 conector de auriculares", true, false, 7);
+
+INSERT INTO Control VALUE (3, true, "azul", "Jack 3.5 mm, USB-C", true, true, 8);
+
+-- ---------------------------ACCESORIOS--------------------------------------
+
+SELECT * FROM accesorio;
+
+INSERT INTO Accesorio VALUE (1, "Plastico ABS", 9);
+
+INSERT INTO Accesorio VALUE (2, "Plastico ABS", 10);
+
+INSERT INTO Accesorio VALUE(3, "Plastico ABS", 11);
+
+-- -----------------FOTOS-----------------------------------------------
+
+INSERT INTO foto VALUE (1, "https://http2.mlstatic.com/D_NQ_NP_644830-MLA50222950634_062022-O.webp", 1);
+INSERT INTO foto VALUE (2, "https://ss423.liverpool.com.mx/lg/1050047756_1p.jpg", 1);
+INSERT INTO foto VALUE (3, "https://ss423.liverpool.com.mx/lg/1050047756_2p.jpg", 1);
+INSERT INTO foto VALUE (4, "https://ss423.liverpool.com.mx/lg/1050047756_3p.jpg", 1);
+INSERT INTO foto VALUE (5, "https://cdn.gameplanet.com/wp-content/uploads/2022/11/24121746/resident-evil-4-remake-ps5.jpg?_ga=2.50641257.1914745646.1679197085-1128018023.1679197085&_gl=1*n81248*_ga*MTEyODAxODAyMy4xNjc5MTk3MDg1*_ga_V97PLX1VDX*MTY3OTIwNTY4Ni4yLjEuMTY3OTIwNTcxMC4wLjAuMA..", 2);
+INSERT INTO foto VALUE (6, "https://cdn.gameplanet.com/wp-content/uploads/2022/11/20161621/ss-resident-evil-1.jpg?_ga=2.50641257.1914745646.1679197085-1128018023.1679197085&_gl=1*n81248*_ga*MTEyODAxODAyMy4xNjc5MTk3MDg1*_ga_V97PLX1VDX*MTY3OTIwNTY4Ni4yLjEuMTY3OTIwNTcxMC4wLjAuMA..", 2);
+INSERT INTO foto VALUE (7, "https://cdn.gameplanet.com/wp-content/uploads/2022/11/20161624/ss-resident-evil-2.jpg?_ga=2.54957419.1914745646.1679197085-1128018023.1679197085&_gl=1*1z0yp3q*_ga*MTEyODAxODAyMy4xNjc5MTk3MDg1*_ga_V97PLX1VDX*MTY3OTIwNTY4Ni4yLjEuMTY3OTIwNTcxMC4wLjAuMA..", 2);
+INSERT INTO foto VALUE (8, "https://http2.mlstatic.com/D_NQ_NP_714411-MLA52696806401_122022-O.webp", 3);
+INSERT INTO foto VALUE (9, "https://http2.mlstatic.com/D_NQ_NP_802679-MLA52696885129_122022-O.webp", 3);
+INSERT INTO foto VALUE (10 ,"https://http2.mlstatic.com/D_NQ_NP_995465-MLA45731835097_042021-O.webp", 4);
+INSERT INTO foto VALUE (11 ,"https://http2.mlstatic.com/D_NQ_NP_903613-MLA45731831377_042021-O.webp", 4);
+INSERT INTO foto VALUE (12, "https://http2.mlstatic.com/D_NQ_NP_903613-MLA45731831377_042021-O.webp", 4);
+INSERT INTO foto VALUE (13, "https://http2.mlstatic.com/D_NQ_NP_858605-MLA45308148746_032021-O.webp", 5);
+INSERT INTO foto VALUE (14, "https://http2.mlstatic.com/D_NQ_NP_679366-MLA45308148747_032021-O.webp", 5);
+INSERT INTO foto VALUE (15, "https://ss423.liverpool.com.mx/xl/1130520908-3p.jpg", 5);
+INSERT INTO foto VALUE(16, "https://ss423.liverpool.com.mx/lg/1106013906.jpg", 6);
+INSERT INTO foto VALUE(17, "https://ss423.liverpool.com.mx/lg/1106013906_1p.jpg", 6);
+INSERT INTO foto VALUE(18, "https://ss423.liverpool.com.mx/lg/1106013906_3p.jpg", 6);
+INSERT INTO foto VALUE(19, "https://cdn.gameplanet.com/wp-content/uploads/2022/09/05041000/control-nacon-xbox-1-247x305.jpg", 7);
+INSERT INTO foto VALUE(20, "https://cdn.gameplanet.com/wp-content/uploads/2022/09/05041013/control-nacon-xbox-3-247x305.jpg", 7);
+INSERT INTO foto VALUE(21, "https://http2.mlstatic.com/D_NQ_NP_972220-MLM53103554750_122022-O.webp", 8);
+INSERT INTO foto VALUE(22, "https://http2.mlstatic.com/D_Q_NP_797145-MLM53103554748_122022-R.webp", 8);
+INSERT INTO foto VALUE(23, "https://http2.mlstatic.com/D_Q_NP_686797-MLM53103554749_122022-R.webp", 8);
+INSERT INTO foto VALUE(24, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/02184247/0631145843407-base-carga-redlemon-playstation-1-510x630.jpg", 9);
+INSERT INTO foto VALUE(25, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/02184249/0631145843407-base-carga-redlemon-playstation-2-510x630.jpg", 9);
+INSERT INTO foto VALUE(26, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/02184252/0631145843407-base-carga-redlemon-playstation-4-510x630.jpg", 9);
+INSERT INTO foto VALUE(27, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/09174438/850021579517-base-carga-voltedge-xbox-1-510x630.jpg", 10);
+INSERT INTO foto VALUE(28, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/09174440/850021579517-base-carga-voltedge-xbox-2-510x630.jpg", 10);
+INSERT INTO foto VALUE(29, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/09174442/850021579517-base-carga-voltedge-xbox-3-510x630.jpg", 10);
+INSERT INTO foto VALUE(30, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/03115359/0631145841373-base-carga-4-joy-con-redlemon-1-510x630.jpg", 11);
+INSERT INTO foto VALUE(31, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/03115401/0631145841373-base-carga-4-joy-con-redlemon-2-510x630.jpg", 11);
+INSERT INTO foto VALUE(32, "https://cdn.gameplanet.com/wp-content/uploads/2023/03/03115404/0631145841373-base-carga-4-joy-con-redlemon-4-510x630.jpg", 11);
+
+select * FROM foto;
