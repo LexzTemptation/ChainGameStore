e4f6f5beb51216bab4be76595dfb14b63e458666
LexzTemptation <alejandro_temptation@outlook.com>
2023-04-12 11:24:30 -0600

    Una wea de font awesome

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faArrowLeft, faCheckSquare} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 export default function Login() {
     return (
