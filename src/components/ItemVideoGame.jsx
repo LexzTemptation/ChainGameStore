b8d92c97de4614f17379bdefe1fe5e8a38cb5e38
LexzTemptation <alejandro_temptation@outlook.com>
2023-04-13 23:34:37 -0600

    Card de videojuego lista

--- src/components/ItemVideoGame.jsx
+++ src/components/ItemVideoGame.jsx
@@ -1,11 +1,14 @@
-
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import "./ItemVideoGame.css"
 
 
 function ItemVideoGame({data, setAccion, setDataDetail}){
 
     let {genero} = data
-    let {titulo} = data["producto"]
+    let {titulo, precio, descripcion} = data["producto"]
+    let { foto } = data["listaFotos"][0]
 
     const handlerDetail = () => {
 
@@ -15,11 +18,35 @@ function ItemVideoGame({data, setAccion, setDataDetail}){
     }
 
     return(
-        <div onClick={handlerDetail} className="container-game">
+        <div>
+            {/* <div onClick={handlerDetail} className="container-game">
 
-            <p>{titulo}</p>
-            <p>{genero}</p>
+                <p>{titulo}</p>
+                <p>{genero}</p>
 
+            </div> */}
+            <div onClick={handlerDetail}>
+                <Row className='rows'>
+                    <Col className='columns'>
+                        <Card class="card" style={{ width: '18rem', height: '480px' }}>
+                            <Card.Img class="card-img-top" variant="top" src={foto} alt="" />
+                            <Card.Body class="card-body">
+                                <Card.Title class="card-title">{titulo}</Card.Title>
+                                <Card.Text class="card-text">
+                                    {genero}
+                                    <details>
+                                        <summary>Ver más...</summary>
+                                        {descripcion}
+                                    </details>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <small className="text-muted">${precio}</small>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         </div>
     )
 }
